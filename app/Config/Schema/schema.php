a4ec74e6a44ffe8b4f29e2a5fe683f86b15ae16b
obamenandro <obamenandro@gmail.com>
2018-03-10 01:50:26 +0800

    develop

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -87,4 +87,17 @@ class AppSchema extends CakeSchema {
         ),
         'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
     );
+
+    public $alcaldezs = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+        'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'year_services' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'position' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => 1)
+        ),
+        'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+    );
 }
